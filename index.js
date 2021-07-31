f98761580deb1fc05ea11a705d9b8710613cf647
salma tarek <62105437+salmaatarekk@users.noreply.github.com>
2021-07-31 16:55:19 +0200

    handle post requests and input validation

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+const Joi = require('joi');
 const express = require('express');
 const app = express();
+app.use(express.json()); 
+
 
 const courses = [
     { id: 1, name: 'course1'},
@@ -14,6 +17,36 @@ app.get( '/', (request, respond) => {
 app.get( '/api/courses', (request, respond) => {
 
     respond.send( courses );
+});
+
+
+app.post('/api/courses', (req, res) => {
+
+    // Validate the input first
+    const schema = {
+        name : Joi.string().min(3).required()
+    }
+    const result = Joi.validate(req.body, schema);
+    if(result.error)
+    {
+        res.status(404).send(result.error.details[0].message);
+        return;
+    }
+
+    //if ( !req.body.name || req.body.name.length < 3) 
+    //{
+        // 400 bad request
+      //  res.status(400).send('Name is required and should be minimum 3 characters');
+        //return;
+   // }
+
+    const course = {
+        id : courses.length + 1,
+        name : req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+
 });
 
 app.get('/api/courses/:id', (request, respond) => {
